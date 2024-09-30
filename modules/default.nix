9eab35d6d7b9697a4fbada0a2bc6e1669b70203b
Alice Carroll <git@alice-carroll.pet>
2024-09-30 21:20:11 +0300

    fix: default cachix settings

--- modules/default.nix
+++ modules/default.nix
@@ -61,6 +61,7 @@ in
             enable = lib.mkEnableOption "cachix";
             pathsToPush = lib.mkOption {
               type = types.nullOr (types.listOf types.package);
+              default = null;
             };
           };
         };
@@ -81,7 +82,9 @@ in
           double = system;
           os = cfg.platform;
           enableCachix = cfg.cachix.enable;
-          pathsToPush = lib.concatStringsSep " " cfg.cachix.pathsToPush;
+          pathsToPush =
+            if cfg.cachix.pathsToPush == null then "" else lib.concatStringsSep " " cfg.cachix.pathsToPush;
+          skipPush = cfg.cachix.pathsToPush == [ ];
         }) ghaSystems;
       };
   };
