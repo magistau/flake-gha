de86c77b299d2bcc4891d644229736de64d52d95
Alice Carroll <git@alice-carroll.pet>
2024-10-03 18:56:32 +0300

    fix: allow null pushFilter

--- modules/default.nix
+++ modules/default.nix
@@ -76,7 +76,7 @@ in
           type = types.str;
         };
         pushFilter = lib.mkOption {
-          type = types.str;
+          type = types.nullOr types.str;
           default = null;
         };
         extraCaches = lib.mkOption {
