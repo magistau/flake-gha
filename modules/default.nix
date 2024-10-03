f6e0d217e1e40d50033f4b7efe375d10726b6a80
Alice Carroll <git@alice-carroll.pet>
2024-10-03 05:57:22 +0300

    feat(cachix): support pushFilter

--- modules/default.nix
+++ modules/default.nix
@@ -75,6 +75,10 @@ in
         cacheName = lib.mkOption {
           type = types.str;
         };
+        pushFilter = lib.mkOption {
+          type = types.str;
+          default = null;
+        };
       };
       checkAllSystems = lib.mkOption {
         type = types.bool;
@@ -97,6 +101,7 @@ in
         config = {
           inherit (globalCfg) checkAllSystems;
           cacheName = lib.optionalString globalCfg.cachix.enable globalCfg.cachix.cacheName;
+          pushFilter = lib.optionalString (globalCfg.cachix.pushFilter != null) globalCfg.cachix.pushFilter;
           matrix = lib.mapAttrsToList (system: cfg: {
             double = system;
             os = cfg.platform;
